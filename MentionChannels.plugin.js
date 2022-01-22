efac960d14f69c3b4e1a1b9b0f8d786881723dbf
Echological <setheweintraub@me.com>
2022-01-22 12:16:15 -0800

    Discord & Zeres Update

--- MentionChannels.plugin.js
+++ MentionChannels.plugin.js
@@ -1,106 +1,153 @@
 /**
  * @name MentionChannels
  * @author Echology
- * @version 3.0.1
+ * @version 4.0.0
  */
 
 const config = {
-    info: {
-        name: "MentionChannels",
-        authors: [
-            {
-                name: "Echology",
-                discord_id: "272875632088842240",
-            }
-        ],
-        version: "3.0.1",
-        description: "Adds a button that puts the mention for the channel clicked in your message, like Discord does for users.",
+  info: {
+    name: "MentionChannels",
+    authors: [
+      {
+        name: "Echology",
+        discord_id: "272875632088842240",
+      },
+    ],
+    version: "4.0.0",
+    description:
+      "Adds a button that puts the mention for the channel clicked in your message, like Discord does for users.",
+  },
+  changelog: [
+    {
+      title: "Zeres & Discord Update",
+      items: ["Fixed plugin to work with the new updates :)"],
     },
-    changelog: [
-        {
-            "title": "Fix Thingy", 
-            "items":[
-                "Something broke, so I unbroke it"
-            ]
-        }
-    ]   
+  ],
 };
 
-module.exports = !global.ZeresPluginLibrary ? class {
-
-    constructor() {
+module.exports = !global.ZeresPluginLibrary
+  ? class {
+      constructor() {
         this._config = config;
-    }
+      }
 
-    load() {
-        BdApi.showConfirmationModal("Library Missing", `The library plugin needed for ${config.info.name} is missing. Please click Download Now to install it.`, {
+      load() {
+        BdApi.showConfirmationModal(
+          "Library Missing",
+          `The library plugin needed for ${config.info.name} is missing. Please click Download Now to install it.`,
+          {
             confirmText: "Download Now",
             cancelText: "Cancel",
             onConfirm: () => {
-                require("request").get("https://rauenzi.github.io/BDPluginLibrary/release/0PluginLibrary.plugin.js", async (error, _response, body) => {
-                    if (error) return require("electron").shell.openExternal("https://betterdiscord.net/ghdl?url=https://raw.githubusercontent.com/rauenzi/BDPluginLibrary/master/release/0PluginLibrary.plugin.js");
-                    await new Promise(r => require("fs").writeFile(require("path").join(BdApi.Plugins.folder, "0PluginLibrary.plugin.js"), body, r));
-                });
-            }
-        });
+              require("request").get(
+                "https://rauenzi.github.io/BDPluginLibrary/release/0PluginLibrary.plugin.js",
+                async (error, _response, body) => {
+                  if (error)
+                    return require("electron").shell.openExternal(
+                      "https://betterdiscord.net/ghdl?url=https://raw.githubusercontent.com/rauenzi/BDPluginLibrary/master/release/0PluginLibrary.plugin.js"
+                    );
+                  await new Promise((r) =>
+                    require("fs").writeFile(
+                      require("path").join(
+                        BdApi.Plugins.folder,
+                        "0PluginLibrary.plugin.js"
+                      ),
+                      body,
+                      r
+                    )
+                  );
+                }
+              );
+            },
+          }
+        );
+      }
+
+      start() {}
+
+      stop() {}
     }
+  : (([Plugin, Library]) => {
 
-    start() { }
-
-    stop() { }
-} : (([Plugin, Library]) => {
-
-    const reg = /(ChannelList)(.*)(ContextMenu)/gm
-
-    const { Patcher, WebpackModules, DCM } = Library;
-
-    const toPatch = WebpackModules.getModules(m => reg.test(m?.default?.displayName))
-
-    const Permissions = BdApi.findModuleByProps("Permissions", "ActivityTypes").Permissions; 
-    const ChannelPermissionUtils = BdApi.findModuleByProps("can", "canEveryone");
-    const UserStore = BdApi.findModuleByProps("getCurrentUser");
-    const ChannelStore = BdApi.findModuleByProps("getDMFromUserId", "getChannel");
-    const LastChannelStore = BdApi.findModuleByProps("getLastSelectedChannelId", "getChannelId");
-
-    return class MentionChannels extends Plugin {
+      const { Patcher, ContextMenu: DCM } = Library;
+      const { Permissions } = BdApi.findModuleByProps(
+          "Permissions",
+          "ActivityTypes"
+        ),
+        { SEND_MESSAGES } = Permissions;
+      const { can } = BdApi.findModuleByProps("can", "canEveryone");
+      const { getCurrentUser } = BdApi.findModuleByProps("getCurrentUser");
+      const { getChannel } = BdApi.findModuleByProps(
+        "getDMFromUserId",
+        "getChannel"
+      );
+      const { getChannelId } = BdApi.findModuleByProps(
+        "getLastSelectedChannelId",
+        "getChannelId"
+      );
+      const { ComponentDispatch } =
+        BdApi.findModuleByProps("ComponentDispatch");
+      return class MentionChannels extends Plugin {
         constructor() {
-            super();
+          super();
         }
 
         async onStart() {
-            this.patch()
+          await this.patcher();
         }
 
-        patch() {
+        async patcher() {
+          const patch = (_, [props], component) => {
+            const { children } = component.props;
 
-            for(var item of toPatch) {
-                Patcher.after(item, "default", (_, [props], component) => {
-                    let channel = props.channel
-
-                    if (ChannelPermissionUtils.can(Permissions.SEND_MESSAGES, UserStore.getCurrentUser().id, ChannelStore.getChannel(LastChannelStore.getChannelId()))) {
-                        let item = DCM.buildMenuItem({
-                            label: "Mention",
-                            type: "text",
-                            action: () => {
-                                BdApi.findModuleByProps('ComponentDispatch').ComponentDispatch.dispatchToLastSubscribed("INSERT_TEXT", {
-                                    content: "<#"+channel.id+">"
-                                })
-                            }
-                        })
-                        component.props.children.unshift(item)
-                    }
+            if (
+              can(
+                SEND_MESSAGES,
+                getCurrentUser().id,
+                getChannel(getChannelId())
+              )
+            ) {
+              children.unshift(
+                DCM.buildMenuItem({
+                  type: "text",
+                  label: "Mention",
+                  action: () => {
+                    ComponentDispatch.dispatchToLastSubscribed("INSERT_TEXT", {
+                      content: "<#" + props.channel.id + ">",
+                    });
+                  },
                 })
-
+              );
             }
+          };
 
+          DCM.getDiscordMenu(
+            (m) =>
+              m?.displayName == "ChannelListTextChannelContextMenu" &&
+              m.toString().includes("AnalyticsLocations.CONTEXT_MENU")
+          ).then((m) => {
+            Patcher.after(m, "default", patch);
+            DCM.forceUpdateMenus();
+          });
+
+          DCM.getDiscordMenu(
+            (m) =>
+              m?.displayName == "ChannelListVoiceChannelContextMenu" &&
+              m.toString().includes("AnalyticsLocations.CONTEXT_MENU")
+          ).then((m) => {
+            Patcher.after(m, "default", patch);
+            DCM.forceUpdateMenus();
+          });
+
+          DCM.forceUpdateMenus();
         }
 
         onLoad() {
+          DCM.forceUpdateMenus();
         }
 
         onStop() {
-            Patcher.unpatchAll();
+          Patcher.unpatchAll();
         }
-
-    }
-})(global.ZeresPluginLibrary.buildPlugin(config));
+      };
+    })(global.ZeresPluginLibrary.buildPlugin(config));
